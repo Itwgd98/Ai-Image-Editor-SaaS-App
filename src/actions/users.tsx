30e34a8f6e92430817cae445e7d87520fe85b722
Gagan Dixit <148959208+Itwgd98@users.noreply.github.com>
2025-11-02 10:59:35 +0530

    Update users.tsx

--- src/actions/users.tsx
+++ src/actions/users.tsx
@@ -18,24 +18,4 @@ export async function getUserCredits() {
   });
 
   return user.credits;
-}"use server";
-
-import { auth } from "~/lib/auth";
-
-import { headers } from "next/headers";
-import { db } from "~/server/db";
-
-export async function getUserCredits() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session) return null;
-
-  const user = await db.user.findUniqueOrThrow({
-    where: { id: session.user.id },
-    select: { credits: true },
-  });
-
-  return user.credits;
 }
